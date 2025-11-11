c15ad7b61f7c3416b471b798f5d04fb6923d6936
jae woo Jung <awfjol2008@gmail.com>
2025-11-11 17:31:01 +0900

    add User entity with fields for username, status, and last login date

---
+++ src/main/java/com/example/springbatchpractice/domain/User.java
@@ -0,0 +1,33 @@
+package com.example.springbatchpractice.domain;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+
+    @Enumerated(EnumType.STRING)
+    private UserStatus status;
+
+    private LocalDateTime lastLoginDate;
+
+    public void changeStatusToDormant() {
+        this.status = UserStatus.DORMANT;
+    }
+}
